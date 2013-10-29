a9e2b8478c742854b1148be5834e65cab383a9b4
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-10-29 16:13:14 -0700

    Added skeletal iterator on Corpus and added new classes to represent different types of sentence level information

--- src/main/java/edu/washington/multir/corpus/Corpus.java
+++ src/main/java/edu/washington/multir/corpus/Corpus.java
@@ -1,15 +1,70 @@
 package edu.washington.multir.corpus;
 
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
 
+import edu.stanford.nlp.pipeline.Annotation;
 import edu.washington.multir.database.CorpusDatabase;
 
 public abstract class Corpus {
 	
-	CorpusDatabase cd;
-	public Corpus(boolean load, boolean train) throws SQLException{
+	private CorpusDatabase cd;
+	private SentenceInformationI[] sentenceInformationTypes;
+	private DocumentIterator di;
+	protected Corpus(SentenceInformationI[] sentenceInformationTypes, boolean load, boolean train) throws SQLException{
+	  this.sentenceInformationTypes = sentenceInformationTypes;
       cd = load ? CorpusDatabase.loadCorpusDatabase(train) : CorpusDatabase.newCorpusDatabase(getSentenceTableSQLSpecification(), getDocumentTableSQLSpecification(), train);
+      this.di = new DocumentIterator();
 	}
-	abstract String getSentenceTableSQLSpecification();
-	abstract String getDocumentTableSQLSpecification();
+	private String getSentenceTableSQLSpecification(){
+		StringBuilder sqlTableSpecificationBuilder = new StringBuilder();
+		sqlTableSpecificationBuilder.append("( SENTID int, ");
+		for(SentenceInformationI sentenceInformation : sentenceInformationTypes){
+			String columnName = sentenceInformation.getClass().getName();
+		}
+		sqlTableSpecificationBuilder.append("PRIMARY KEY (SENTID))");
+		return sqlTableSpecificationBuilder.toString();
+	}
+	private String getDocumentTableSQLSpecification(){
+		StringBuilder sqlTableSpecificationBuilder = new StringBuilder();
+		sqlTableSpecificationBuilder.append("( DOCNAME VARCHAR(128),");
+		
+		sqlTableSpecificationBuilder.append("PRIMARY KEY (DOCNAME))");
+		return sqlTableSpecificationBuilder.toString();
+	}
+	
+    private class DocumentIterator implements Iterator<Annotation>{
+    	
+    	String currentDocument = cd.firstDocument;
+		@Override
+		public boolean hasNext() {
+			if(currentDocument != null){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+		@Override
+		public Annotation next() {
+			//query for first document and build Annotation
+			
+			
+			currentDocument = cd.nextDocument(currentDocument);
+		}
+
+		@Override
+		public void remove() {
+			// TODO Auto-generated method stub
+			
+		}    	
+    }
+    
+    public DocumentIterator getDocumentIterator(){
+    	return this.di;
+    }
+    
+    
 }
--- src/main/java/edu/washington/multir/corpus/DefaultCorpus.java
+++ src/main/java/edu/washington/multir/corpus/DefaultCorpus.java
@@ -4,18 +4,11 @@ import java.sql.SQLException;
 
 public class DefaultCorpus extends Corpus {
 	
+	//describe all the information types involed in this corpus type
+	private static SentenceInformationI[] sentenceInformationTypes = {new sentenceTokenInformation()};
+	
 	public DefaultCorpus(boolean load, boolean train) throws SQLException{
-		super(load,train);
+		super(sentenceInformationTypes,load,train);
 	}
-
-	@Override
-	final String getSentenceTableSQLSpecification() {
-		return "(sentenceId int, docName VARCHAR(3200), text VARCHAR(32000))";
-	}
-
-	@Override
-	final String getDocumentTableSQLSpecification() {
-		return "(docName VARCHAR(3200), coref VARCHAR(3200)";
-	}
-
+	
 }
